ad77ea6bda277acf7ec6bc496afac4c9b9a0e3d2
Real <hassanzahidreal@gmail.com>
2023-01-04 17:16:26 +0600

    saving to github

--- src/componemts/header/header.jsx
+++ src/componemts/header/header.jsx
@@ -12,7 +12,7 @@ import HeaderSocial from './HeaderSocials'
       <div className="container header__container">
          <h5>Hello I'm </h5>
         <h1>MD.Zahid Hassan</h1>
-      <h5 className="text-light">Fullstack Developer </h5>
+      <h5 className="text-light">Fullstack web Developer </h5>
       <CTA/>
       <HeaderSocial/>
       <div className='me'>
